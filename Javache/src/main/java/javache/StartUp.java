a68654170fd29ce406411a4665c3b99bc74d30bf
MartinBG <MartinBG@abv.bg>
2018-05-22 09:27:59 +0300

    Refactoring

--- Javache/src/main/java/javache/StartUp.java
+++ Javache/src/main/java/javache/StartUp.java
@@ -1,15 +1,20 @@
 package javache;
 
-import javache.constants.WebConstants;
+import javache.constants.ServerConstants;
 import javache.server.Server;
 
 import java.io.IOException;
 
 public class StartUp {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        start(args);
+    }
 
-        final Server server = new Server(WebConstants.SERVER_PORT);
+    private static void start(final String[] args) {
+        final int port = (args.length > 1) ? Integer.parseInt(args[1]) : ServerConstants.SERVER_PORT;
+
+        final Server server = new Server(port);
 
         try {
             server.run();
