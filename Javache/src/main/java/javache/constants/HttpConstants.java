0adbcc67547cdce41c04cc7baedca6cf8b3ff781
MartinBG <MartinBG@abv.bg>
2018-05-22 10:30:10 +0300

    constants

--- Javache/src/main/java/javache/constants/HttpConstants.java
+++ Javache/src/main/java/javache/constants/HttpConstants.java
@@ -1,8 +1,13 @@
 package javache.constants;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public final class HttpConstants {
 
     public static final String SERVER_HTTP_VERSION = "HTTP/1.1";
+
+    public static final Charset SERVER_ENCODING = StandardCharsets.UTF_8;
 
     public static final String SEPARATOR_LINE = "\\r\\n";
     public static final String SEPARATOR_EMPTY_SPACE = " ";
@@ -11,6 +16,11 @@ public final class HttpConstants {
     public static final String SEPARATOR_BODY_KVP = "=";
     public static final String SEPARATOR_DOT = ".";
     public static final String SEPARATOR_LINE_RESPONSE = System.lineSeparator();
+    public static final String LOCATION = "Location";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_LENGTH = "Content-Length";
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
+    public static final String INLINE_CONTENT_DISPOSITION = "inline";
 
     private HttpConstants() {
     }
--- Javache/src/main/java/javache/constants/ServerConstants.java
+++ Javache/src/main/java/javache/constants/ServerConstants.java
@@ -9,6 +9,7 @@ public final class ServerConstants { // TODO - export to property file
     private static final String RESOURCE_FOLDER_PATH = System.getProperty("user.dir") + "/src/main/resources/";
     public static final String ASSETS_FOLDER_PATH = RESOURCE_FOLDER_PATH + "assets/";
     public static final String PAGES_FOLDER_PATH = RESOURCE_FOLDER_PATH + "pages/";
+    public static final String INDEX_PAGE = "/index";
 
     private ServerConstants() {
     }
