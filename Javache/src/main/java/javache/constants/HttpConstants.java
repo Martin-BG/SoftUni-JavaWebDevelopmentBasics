6e2542e9b9e8c018cd05ef33d85ca83b4ee2806b
MartinBG <MartinBG@abv.bg>
2018-05-27 01:13:17 +0300

    Constants refactoring

--- Javache/src/main/java/javache/constants/HttpConstants.java
+++ Javache/src/main/java/javache/constants/HttpConstants.java
@@ -1,13 +1,6 @@
 package javache.constants;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 public final class HttpConstants {
-
-    public static final String SERVER_HTTP_VERSION = "HTTP/1.1";
-
-    public static final Charset SERVER_ENCODING = StandardCharsets.UTF_8;
 
     public static final String SEPARATOR_LINE = "\\r\\n";
     public static final String SEPARATOR_EMPTY_SPACE = " ";
@@ -16,16 +9,20 @@ public final class HttpConstants {
     public static final String SEPARATOR_BODY_KVP = "=";
     public static final String SEPARATOR_DOT = ".";
     public static final String SEPARATOR_LINE_RESPONSE = System.lineSeparator();
+
     public static final String LOCATION = "Location";
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String CONTENT_LENGTH = "Content-Length";
     public static final String CONTENT_DISPOSITION = "Content-Disposition";
     public static final String INLINE_CONTENT_DISPOSITION = "inline";
+
     public static final String COOKIE = "Cookie";
     public static final String SEPARATOR_COOKIES = "; ";
     public static final String SEPARATOR_COOKIE_KVP = "=";
     public static final String COOKIE_RESPONSE_NAME = "Set-Cookie: ";
     public static final String COOKIE_DELETE = "deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+
+    public static final String JAVACHE_SESSION_KEY = "Javache";
 
     private HttpConstants() {
     }
